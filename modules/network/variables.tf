b9abed3f031d5564f015aeee181b28ba9b46ddbe
jatin <malhotra.jatin21.com>
2025-06-01 18:25:10 +0530

    Your commit message here

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,11 +1,3 @@
 variable "compartment_ocid" {}
-variable "display_name" {
-    description = "The display name for the VCN"
-    type        = string
-    default     = "example_vcn44"
-}
-variable "cidr_block" {
-    description = "The CIDR block for the VCN"
-    type        = string
-    default     = "10.2.0.0/16"
-}
+variable "display_name" {}
+variable "cidr_block" {}
--- variables.tf
+++ variables.tf
@@ -4,5 +4,13 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
-variable "display_name" {}
-variable "cidr_block" {}
+variable "cidr_block" {
+    description = "The CIDR block for the VCN"
+    type        = string
+    default     = "10.2.0.0/16"
+}
+variable "display_name" {
+    description = "The name of the VCN"
+    type        = string
+    default     = "example_vcn44"
+}
